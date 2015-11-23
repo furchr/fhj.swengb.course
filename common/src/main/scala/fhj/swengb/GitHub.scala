9a812b611607e401341ab0b75ca9568ca7bb8b3d
carinaher <carina.herzog@gmail.com>
2015-11-23 15:57:56 +0100

    extended user-method

--- common/src/main/scala/fhj/swengb/GitHub.scala
+++ common/src/main/scala/fhj/swengb/GitHub.scala
@@ -9,7 +9,7 @@ import spray.json._
   */
 object GitHub {
 
-  case class User(login: String, avatarUrl: URL)
+  case class User(login: String, avatarUrl: URL, htmlUrl: URL, fullname: String, email: String, blogURL: String, pubRepos: String, pubGists: String, createdOn: String, updOn: String )
 
   object GithubUserProtocol extends DefaultJsonProtocol {
 
@@ -17,7 +17,15 @@ object GitHub {
       def write(user: User): JsValue =
         JsArray(
           JsString(user.login),
-          JsString(user.avatarUrl.toString)
+          JsString(user.avatarUrl.toString),
+          JsString(user.htmlUrl.toString),
+          JsString(user.fullname),
+          JsString(user.email),
+          JsString(user.blogURL),
+          JsString(user.pubRepos),
+          JsString(user.pubGists),
+          JsString(user.createdOn),
+          JsString(user.updOn)
         )
 
 
@@ -26,7 +34,15 @@ object GitHub {
           case JsObject(m) =>
             val JsString(login) = m("login")
             val JsString(a_url) = m("avatar_url")
-            User(login, new URL(a_url))
+            val JsString(html_url) = m("html_url")
+            val JsString(f_name) = m("name")
+            val JsString(e_mail) = m("email")
+            val JsString(blog_url) = m("blog")
+            val JsString(p_repos) = m("public_repos")
+            val JsString(p_gists) = m("public_gists")
+            val JsString(cr_on) = m("created_at")
+            val JsString(upd_on) = m("updated_at")
+            User(login, new URL(a_url), new URL(html_url), f_name, e_mail, blog_url, p_repos, p_gists, cr_on, upd_on)
           case x =>
             deserializationError("GitHubUser expected.")
         }
