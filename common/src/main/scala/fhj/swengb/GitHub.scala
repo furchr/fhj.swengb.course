1934dba5d512615dd8809716e0343b327041ce55
carinaher <carina.herzog@gmail.com>
2015-11-23 19:56:08 +0100

    extended user-method

--- common/src/main/scala/fhj/swengb/GitHub.scala
+++ common/src/main/scala/fhj/swengb/GitHub.scala
@@ -10,6 +10,7 @@ import spray.json._
 object GitHub {
 
   case class User(login: String,
+                  id: BigDecimal,
                   avatarUrl: URL,
                   htmlUrl: URL,
                   name: String,
@@ -25,6 +26,7 @@ object GitHub {
       def write(user: User): JsValue =
         JsArray(
           JsString(user.login),
+          JsNumber(user.id),
           JsString(user.avatarUrl.toString),
           JsString(user.htmlUrl.toString),
           JsString(user.name),
@@ -39,6 +41,7 @@ object GitHub {
         value match {
           case JsObject(m) =>
             val JsString(login) = m("login")
+            val JsNumber(id) = m("id")
             val JsString(a_url) = m("avatar_url")
             val JsString(html_url) = m("html_url")
             val JsString(f_name) = m("name")
@@ -46,7 +49,7 @@ object GitHub {
             val JsNumber(p_repos) = m("public_repos")
             val JsNumber(followers) = m("followers")
             val JsNumber(following) = m("following")
-            User(login, new URL(a_url), new URL(html_url), f_name, /*company,*/ p_repos , followers, following)
+            User(login, id, new URL(a_url), new URL(html_url), f_name, /*company,*/ p_repos , followers, following)
           case x =>
             deserializationError("GitHubUser expected.")
         }
